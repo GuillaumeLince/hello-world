361e90f892ab09ecac827002af103b9c681792c7
GuillaumeLince <glince1707@gmail.com>
2016-10-10 18:49:32 +0200

    HelloWorld main class

--- helloWorld/src/main/java/Hello.java
+++ helloWorld/src/main/java/Hello.java
@@ -2,6 +2,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("HelloWorld from GL-Branch");
+        System.out.println("HelloWorld from GL-Branch for pull request");
     }
 }
